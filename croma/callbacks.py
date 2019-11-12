65e513c39ce01f7c75422ba7d6cd38961de3616e
Fidel Echevarria Corrales <fidelpuerto92@gmail.com>
2019-11-12 23:33:15 +0100

    Added radius callback example

--- croma/callbacks.py
+++ croma/callbacks.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-pos_dot = np.array([0.1, 0.1, 0.0])
+pos_dot = np.array([0.1, 0.1, 0.1])
 pos = np.array([0., 0., 0.]) 
 def centerCallback(dt):
     global pos
     global pos_dot
     pos = pos + pos_dot * dt
     return pos
+
+r_dot = np.array([0.1])
+r = np.array([0.]) 
+def radiusCallback(dt):
+    global r
+    global r_dot
+    r = r + r_dot * dt
+    return r
